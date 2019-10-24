2ddfdd9e46508f6785bfb33535e511f2bf58a79b
Gum.Albina <Gumerova.albina38@gmail.com>
2019-10-24 21:01:22 +0300

    make average color logic

--- app/src/main/java/edu/spbspu/amd/morze_app/receiver/image_processing/ImageProcessing.java
+++ app/src/main/java/edu/spbspu/amd/morze_app/receiver/image_processing/ImageProcessing.java
@@ -1,11 +1,93 @@
 package edu.spbspu.amd.morze_app.receiver.image_processing;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.RadialGradient;
+import android.util.Pair;
 
 public class ImageProcessing {
+    static class RGB implements Cloneable
+    {
+        public Object clone()
+        {
+            try {
+                return super.clone();
+            }
+            catch (Exception ignored)
+            {
+                return null;
+            }
+        }
+
+        RGB(int r_, int g_, int b_)
+        {
+            r = r_;
+            g = g_;
+            b = b_;
+        }
+
+        void incrementColor(int r_, int g_, int b_)
+        {
+            r += r_;
+            g += g_;
+            b += b_;
+        }
+
+        void devideColors(int devider)
+        {
+            r = r / devider;
+            g = g / devider;
+            b = b / devider;
+        }
+
+        void setZeroColor()
+        {
+            r = 0;
+            g = 0;
+            b = 0;
+        }
+
+        int r;
+        int g;
+        int b;
+    }
+
+    static class AverageColors
+    {
+        AverageColors(RGB prev_, RGB cur_)
+        {
+            prev = prev_;
+            cur = cur_;
+        }
+
+        void setZeroColors()
+        {
+            prev.setZeroColor();
+            cur.setZeroColor();
+        }
+
+        void deviceColors(int devicer)
+        {
+            prev.devideColors(devicer);
+            cur.devideColors(devicer);
+        }
+
+        RGB prev;
+        RGB cur;
+    }
+
+
     private boolean first = true;
     private Bitmap m_prevFrameImage = null;
+    private static int epsilon;
+    private RGB m_prevAverageColor;
     private int m_newAverageColor = -1;
+
+    private static AverageColors m_averageColors;
+    static {
+        m_averageColors = new AverageColors(new RGB(0, 0, 0), new RGB(0, 0, 0 ));
+        epsilon = 100;
+    }
 
     private final int LIMIT_DIFF_PIXEL_NUM = 7000;
     private final int MISMATCH_COLOR = 0x80ff0000;
@@ -53,53 +135,44 @@ public class ImageProcessing {
         return 0xff & color;
     }
 
+    private void getAvarageColor(Bitmap cur) {
+        //prev = last current
+        m_averageColors.prev = (RGB) m_averageColors.cur.clone();
+
+        //count average for new current
+        m_averageColors.cur.setZeroColor();
+
+        int height = cur.getHeight();
+        int width = cur.getWidth();
+
+        int pixels_count = 0;
+        for (int y = 0; y < height; y += 3) {
+            for (int x = 0; x < width; x += 3) {
+                pixels_count++;
+                try {
+                    int pixel = cur.getPixel(x,y);
+                    m_averageColors.cur.incrementColor(Color.red(pixel), Color.green(pixel), Color.blue(pixel));
+                }
+                catch (Exception ignored)
+                {
+                }
+            }
+        }
+
+        m_averageColors.cur.devideColors(pixels_count);
+    }
+
+    private boolean isDifferentColors(RGB c1, RGB c2)
+    {
+        return (Math.abs(c1.r - c2.r) > epsilon)
+                && (Math.abs(c1.g - c2.g) > epsilon)
+                && (Math.abs(c1.b - c2.b) > epsilon);
+    }
+
     private int _isDiffFrom(Bitmap curFrameImage)
     {
-        int curDiffPixelNum = 0;
-        int diffPixelValueSum = 0;
-        int height = curFrameImage.getHeight();
-        int width = curFrameImage.getWidth();
+        getAvarageColor(curFrameImage);
 
-        for (int y = 0; y < height; y+=3) {
-            for (int x = 0; x < width; x+=3) {
-                try {
-                    int pixelC = curFrameImage.getPixel(x, y);
-                    int pixelB = m_prevFrameImage.getPixel(x, y);
-                    if (pixelB == pixelC) {
-                        continue;
-                    }
-
-                    curDiffPixelNum++;
-                    diffPixelValueSum += pixelC;
-                } catch (Exception e) {
-                    // handled height or width mismatch
-                    diffPixelValueSum += MISMATCH_COLOR;
-                }
-            }
-        }
-
-        boolean isDiff = curDiffPixelNum >= LIMIT_DIFF_PIXEL_NUM;
-        if (isDiff) {
-            int oldAverageColor = m_newAverageColor;
-            m_newAverageColor = diffPixelValueSum / curDiffPixelNum;
-
-            if (first) {
-                first = false;
-            } else {
-                int new_r = getColorR(m_newAverageColor);
-                int new_g = getColorG(m_newAverageColor);
-                int new_b = getColorB(m_newAverageColor);
-
-                int r = getColorR(oldAverageColor);
-                int g = getColorG(oldAverageColor);
-                int b = getColorB(oldAverageColor);
-
-                if (Math.abs(new_r - r) <= 100 && Math.abs(new_g - g) <= 100 && Math.abs(new_b - b) <= 100) {
-                    isDiff = false;
-                }
-            }
-        }
-
-        return isDiff ? 1 : 0;
+        return isDifferentColors(m_averageColors.prev, m_averageColors.cur) ? 1 : 0;
     }
 }
